c4ceeb8acde34f7ec04d43004388792aebed636b
InnaL <innal@Macintosh.local>
2015-02-25 13:57:36 +0200

    Add keyboard

--- ecomap/ecomap/PhotoViewController.m
+++ ecomap/ecomap/PhotoViewController.m
@@ -10,14 +10,17 @@
 #import <Foundation/Foundation.h>
 #import <MobileCoreServices/MobileCoreServices.h>
 
-@interface PhotoViewController () <UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@interface PhotoViewController () <UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate, UITextViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) NSMutableArray *imageDescriptions;
+@property (nonatomic, strong) UITextView *messageBox;
+@property(nonatomic, strong) UITextField *activeField;
 
 @end
 
 @implementation PhotoViewController
+
 
 - (void)viewDidLoad
 {
@@ -25,7 +28,67 @@
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
     self.imageDescriptions = [NSMutableArray array];
+    //Set gesture recognizer
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(touchUpinside:)];
+    tap.numberOfTapsRequired = 1;
+    [self.view addGestureRecognizer:tap];
+
 }
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    //setup keyboard notifications
+    [self registerForKeyboardNotifications];
+}
+
+-(void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [self.activeField resignFirstResponder];
+    [self deregisterForKeyboardNotifications];
+}
+
+#pragma mark - keyborad managment
+- (void)registerForKeyboardNotifications
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWasShown:)
+                                                 name:UIKeyboardDidShowNotification object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillBeHidden:)
+                                                 name:UIKeyboardWillHideNotification object:nil];
+    
+}
+
+- (void)deregisterForKeyboardNotifications
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardDidShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardDidHideNotification object:nil];
+}
+
+// Called when the UIKeyboardDidShowNotification is sent
+#define KEYBOARD_TO_TEXTFIELD_SPACE 8.0
+- (void)keyboardWasShown:(NSNotification*)aNotification
+{
+    NSDictionary* info = [aNotification userInfo];
+    CGSize keyboardSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    //Increase scroll view contetn size by keyboard size
+    CGRect contetntViewRect = self.activeField.superview.superview.frame;
+    contetntViewRect.size.height += keyboardSize.height;
+}
+  
+
+#pragma mark - text field delegate
+// Called when the UIKeyboardWillHideNotification is sent
+- (void)keyboardWillBeHidden:(NSNotification*)aNotification
+{
+    NSDictionary* info = [aNotification userInfo];
+    CGSize keyboardSize = [[info objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue].size;
+}
+
 
 - (IBAction)galleryTap:(id)sender
 {
@@ -120,7 +183,44 @@ forRowAtIndexPath:(NSIndexPath *)indexPath
     cell.textLabel.text = descr.imageDescription;
     cell.imageView.image = descr.image;
     
+    UITextField *myTextField = [[UITextField alloc]initWithFrame:CGRectMake(0, 10, 125, 25)];
+    myTextField.adjustsFontSizeToFitWidth = NO;
+    myTextField.backgroundColor = [UIColor clearColor];
+    myTextField.autocorrectionType = UITextAutocorrectionTypeNo;
+    myTextField.autocapitalizationType = UITextAutocapitalizationTypeWords;
+    myTextField.textAlignment = UITextAlignmentRight;
+    myTextField.keyboardType = UIKeyboardAppearanceDefault;
+    myTextField.returnKeyType = UIReturnKeyDone;
+    myTextField.clearButtonMode = UITextFieldViewModeNever;
+    myTextField.delegate = self;
+    cell.accessoryView = myTextField;
+    myTextField.placeholder = @"Add description";
+    
     return cell;
 }
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    
+    return true;
+}
+
+#pragma mark - UITextViewDelegate
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    self.activeField = textField;
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    self.activeField = nil;
+}
+
+- (void)touchUpinside:(UITapGestureRecognizer *)sender {
+    [self.activeField resignFirstResponder];
+    NSLog(@"Tap");
+}
+
 
 @end
