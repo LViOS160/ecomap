572fedcd8ab1a4baf1ab689587d2263b2a3fafb4
Vasyl Kotsiuba <clic@ukr.net>
2015-03-05 12:39:08 +0200

    creates actionSheet VC

---
+++ ecomap/ecomap/UserViewController/LoginActionSheetViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginActionSheetViewController.h
+//  ecomap
+//
+//  Created by Vasilii Kotsiuba on 3/5/15.
+//  Copyright (c) 2015 SoftServe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginActionSheetViewController : UIViewController
+
+@end
