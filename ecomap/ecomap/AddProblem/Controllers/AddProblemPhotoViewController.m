a1e6356c066657f98a838ddef8d466669cd258e7
Inna Labuskaya <innalabynskaya@gmail.com>
2015-03-08 19:05:02 +0200

    added photo view controller to addProblem menu

--- ecomap/ecomap/AddProblem/Controllers/AddProblemPhotoViewController.m
+++ ecomap/ecomap/AddProblem/Controllers/AddProblemPhotoViewController.m
@@ -7,7 +7,29 @@
 //
 
 #import "AddProblemPhotoViewController.h"
+#import "PhotoViewController.h"
+#import "Defines.h"
+
+@interface AddProblemPhotoViewController() <PhotoViewControllerDelegate>
+
+@end
 
 @implementation AddProblemPhotoViewController
 
+- (IBAction)addPhotoTap:(id)sender {
+//    PhotoViewController *viewController = [[PhotoViewController alloc] initWithNibName:@"PhotoViewController" bundle:nil];
+//    viewController.delegate = self;
+//    [self presentViewController:viewController animated:YES completion:nil];
+}
+
+- (void)photoViewControllerDidCancel:(PhotoViewController *)viewController
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)photoViewControllerDidFinish:(PhotoViewController *)viewController withImageDescriptions:(NSArray *)imageDescriptions
+{
+    //imageDescriptions - array of EcomapLocalPhoto
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 @end
