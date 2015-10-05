7d0477b83207d294631b19657d947fa024e87278
admin <admin@macminissu9.training.local>
2015-10-05 14:01:47 +0300

    LVIOSS-139. Add filter "My problems"; Adding cell for new filter.

--- ecomap/ecomap/ProblemFilterTVC.m
+++ ecomap/ecomap/ProblemFilterTVC.m
@@ -200,7 +200,7 @@ static NSString *kproblemOwnerCellID = @"ownerProblemCell";
     UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:kproblemOwnerCellID];
     
     UILabel *titleLabel = (UILabel *)[cell viewWithTag:kTitleTag];
-    titleLabel.text = (indexPath.row == 0) ? NSLocalizedString(@"Нова", @"New") : NSLocalizedString(@"Вирішена", @"Solved");
+    titleLabel.text = NSLocalizedString(@"Відображати тільки мої проблеми", @"Show only my problem");
     
     // Check either filtering mask contains type of the problem of the current row
     // Depending on answer show image.
