2456a29b6248f1ec533e7f3c27fb329c36c88e1f
admin <admin@macminissu4.training.local>
2015-10-26 13:12:36 +0200

    NSFetchedResultController

---
+++ ecomap/ecomap/Model/Value-Object Classes/EcomapFetchedResultController.m
@@ -0,0 +1,31 @@
+//
+//  ProblemFetchResController.m
+//  ecomap
+//
+//  Created by admin on 10/21/15.
+//  Copyright © 2015 SoftServe. All rights reserved.
+//
+
+#import "EcomapFetchedResultController.h"
+#import "AppDelegate.h"
+
+@implementation EcomapFetchedResultController
+
++ (NSFetchRequest*)requestWithEntityName:(NSString*)entityName sortBy:(NSString*)sortDescriptor
+{
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    NSEntityDescription *entity = [NSEntityDescription
+                                   entityForName:entityName
+                                   inManagedObjectContext:[AppDelegate sharedAppDelegate].managedObjectContext];
+    [fetchRequest setEntity:entity];
+    
+    NSSortDescriptor *sort = [[NSSortDescriptor alloc]
+                              initWithKey:sortDescriptor ascending:NO];
+    [fetchRequest setSortDescriptors:[NSArray arrayWithObject:sort]];
+    
+    [fetchRequest setFetchBatchSize:20];
+    
+    return fetchRequest;
+}
+
+@end
